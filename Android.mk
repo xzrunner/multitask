545b6d161b41eba115409f381f15e363fa663cd0
zz <zhuguang@ejoy.com>
2017-09-11 16:01:42 +0800

    fix make for c++11

--- Android.mk
+++ Android.mk
@@ -15,6 +15,8 @@ LOCAL_C_INCLUDES  := \
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -name "*.cpp" -print)) \
 
+LOCAL_CPPFLAGS  := -std=c++11
+
 include $(BUILD_STATIC_LIBRARY)
 
 LOCAL_PATH := $(INNER_SAVED_LOCAL_PATH)
