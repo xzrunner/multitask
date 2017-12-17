7396bbc04e94436f86a7c994ec799807a5ece56b
zz <zhuguang@ejoy.com>
2017-12-17 16:33:53 +0800

    [ADDED] macro to close TaskStat

--- source/TaskStat.cpp
+++ source/TaskStat.cpp
@@ -2,6 +2,8 @@
 
 #include <glp_loop.h>
 
+#define MT_TASK_STAT_OPEN
+
 namespace mt
 {
 
@@ -27,6 +29,7 @@ int TaskStat::RegisterTaskType(const CU_STR& desc)
 
 void TaskStat::Add(std::thread::id thread_id, int type, uint32_t begin, uint32_t end) 
 {
+#ifdef MT_TASK_STAT_OPEN
 	std::lock_guard<std::mutex> lock(m_mutex);
 
 	m_tasks.push_back(Task(thread_id, type, begin, end));
@@ -37,16 +40,19 @@ void TaskStat::Add(std::thread::id thread_id, int type, uint32_t begin, uint32_t
 	if (end > m_time_max) {
 		m_time_max = end;
 	}
+#endif // MT_TASK_STAT_OPEN
 }
 
 void TaskStat::Clear() 
 {
+#ifdef MT_TASK_STAT_OPEN
 	std::lock_guard<std::mutex> lock(m_mutex);
 
 	m_tasks.clear();
 
 	m_time_min = std::numeric_limits<uint32_t>::max();
 	m_time_max = std::numeric_limits<uint32_t>::min();
+#endif // MT_TASK_STAT_OPEN
 }
 
 /************************************************************************/
@@ -57,13 +63,17 @@ TaskStat::Checkpoint::Checkpoint(std::thread::id thread_id, int type)
 	: m_thread_id(thread_id)
 	, m_type(type)
 {
+#ifdef MT_TASK_STAT_OPEN
 	m_begin = glp_get_time();
+#endif // MT_TASK_STAT_OPEN
 }
 
 TaskStat::Checkpoint::~Checkpoint()
 {
+#ifdef MT_TASK_STAT_OPEN
 	uint32_t end = glp_get_time();
 	TaskStat::Instance()->Add(m_thread_id, m_type, m_begin, end);
+#endif // MT_TASK_STAT_OPEN
 }
 
 }
